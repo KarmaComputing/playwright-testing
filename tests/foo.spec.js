019529c61d4a318920203823226889212c38b2b4
Joel <joel@karmacomputing.co.uk>
2021-07-29 18:37:41 -0400

    including screenshots

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -6,17 +6,20 @@ test.describe('tests: ', () => {
   await page.goto('https://playwright.dev/');
   const name = await page.innerText('.navbar__title');
   expect(name).toBe('Playwright');
+  expect(await page.screenshot()).toMatchSnapshot('playwright.png');
   });
 
   test("two", async ({ page }) => {
   await page.goto('https://subscriptionwebsitebuilder.co.uk/');
   const name = await page.innerText('.navbar-brand');
   expect(name).toBe('Subscribie');
+  expect(await page.screenshot()).toMatchSnapshot('subscribie.png');
   });
 
   test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
     await page.goto('https://footballclub.subscriby.shop/');
+    expect(await page.screenshot()).toMatchSnapshot('footballclub-subscribie.png');
   });
 
 });
